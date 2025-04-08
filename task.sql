30c78561843be3d112f9053d131642ff73fd66ed
Kateryna <kateryna.kotyh@gmail.com>
2025-04-08 16:46:24 +0300

    Solution

--- task.sql
+++ task.sql
@@ -2,10 +2,15 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
+INSERT INTO Orders (CustomerID, Date) 
+  VALUES (1, '2023-10-01');
 
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+  VALUES (1, 1, 1);
 -- Start the transaction 
 START TRANSACTION; 
 
 -- And some data should be created inside the transaction 
-
+UPDATE Products SET WarehouseAmount = WarehouseAmount - OrderItems.Count
+WHERE ID = OrderItems.ProductID;
 COMMIT; 
